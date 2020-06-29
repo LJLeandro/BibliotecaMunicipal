7ea78b034a8eb4e629232217dcf8c7f1d5352838
LJLeandro <lucasjleandro@gmail.com>
2020-06-29 00:21:17 -0300

    Ajuste de código

--- routes/routes.ts
+++ routes/routes.ts
@@ -2,17 +2,14 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 // E também utilizaremos as funções exportas do controller
 import { getLivros, getLivro, postLivro, putLivro, deleteLivro } from "../controllers/livros-controller.ts";
+
 const router = new Router();
 
-router.get("/api/v1/livros", getLivros);
-
-router.get("/api/v1/livros/:id", getLivro);
-
-router.post('/api/v1/livros', postLivro);
-
-router.put('/api/v1/livros', putLivro);
-
-router.delete('/api/v1/livros/:id', deleteLivro);
+router.get("/api/v1/livros", getLivros).
+        get("/api/v1/livros/:id", getLivro)
+        .post('/api/v1/livros', postLivro)
+        .put('/api/v1/livros', putLivro)
+        .delete('/api/v1/livros/:id', deleteLivro);
 
 // Exportaremos o Router instanciado e carregado para o arquivo que irá inicializar nossa aplicação.
 export default router;
